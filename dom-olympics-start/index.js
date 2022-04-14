133f8fb991f7578b26486d76cd1e70653386a5cc
Cameron Ford <c.r.ford@outlook.com>
2022-04-13 22:12:40 -0600

    the 1st commit for this project

---
+++ dom-olympics-start/index.js
@@ -0,0 +1,30 @@
+
+var headtextsmall = document.createElement("p");
+headtextsmall.innerHTML = "<span>Cameron<span> wrote this javascript"
+document.body.prepend(headtextsmall)
+headtextsmall.style.textAlign = "center"
+document.querySelector("span").style.color ="blue"
+
+var headtext = document.createElement("p");
+headtext.textContent = "JavaScript Made This!!"
+headtext.style.fontSize = "30px"
+document.body.prepend(headtext); 
+headtext.style.textAlign = "center"
+
+// var button = document.getElementsByTagName("button")
+// document.getElementById("clear-button").addEventListener("click",funClear;
+
+// function funClear () { document.querySelector(".messages").innerHTML("");
+// }
+
+document.querySelector(".l1").textContent="where do you live"
+
+document.getElementsByClassName(messageleft).textContent="with my parents"
+
+document.getElementsByClassName("l2").textContent="where do your parent live"
+
+document.getElementsByClassName("r2").textContent="with me"
+
+
+
+
