a41b806c2746b9aff5d7cd06a1ceb683936d63eb
Cameron Ford <c.r.ford@outlook.com>
2022-04-20 12:16:19 -0600

    now the messages go back and forth on the left and right side of the page with the proper styling for eatch elment

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -1,6 +1,5 @@
-
 var headtextsmall = document.createElement("p");
-headtextsmall.innerHTML = "<span>Cameron<span> wrote this javascript"
+headtextsmall.innerHTML = "<span>Cameron</span> wrote this javascript"
 document.body.prepend(headtextsmall)
 headtextsmall.style.textAlign = "center"
 document.querySelector("span").style.color ="blue"
@@ -19,12 +18,72 @@ headtext.style.textAlign = "center"
 
 document.querySelector(".l1").textContent="where do you live"
 
-document.getElementsByClassName(messageleft).textContent="with my parents"
+document.querySelector(".r1").textContent="with my parents"
 
-document.getElementsByClassName("l2").textContent="where do your parent live"
+document.querySelector(".l2").textContent="where do your parent live"
 
-document.getElementsByClassName("r2").textContent="with me"
+document.querySelector(".r2").textContent="with me"
 
+document.getElementById("clear-button").addEventListener("click",clearfun)
 
+function clearfun () {
+    document.querySelector(".messages").textContent=""
+}
 
+document.getElementById("theme-drop-down").addEventListener("change",dropdownfun)
 
+function dropdownfun() {
+    var theme = document.getElementById("theme-drop-down").value
+    if (theme==="theme-one"){
+        theme1fun()
+    }
+    if (theme==="theme-two") {
+    theme2fun()
+    }
+}
+
+function theme2fun() {
+    var text = document.querySelectorAll(".left")
+    var text2 = document.querySelectorAll(".right")
+    for (let i = 0; i < text.length; i++){
+        text[i].style.backgroundColor="red"
+        text[i].style.color="white"
+    }
+    for (let i = 0; i < text2.length; i++) {
+        text2[i].style.backgroundColor="black"
+        text2[i].style.color="white"
+    }
+} 
+
+function theme1fun() {
+    var text = document.querySelectorAll(".left")
+    var text2 = document.querySelectorAll(".right")
+    for (let i = 0; i < text.length; i++){
+        text[i].style.backgroundColor="blue"
+        text[i].style.color="white"
+    }
+    for (let i = 0; i < text2.length; i++) {
+        text2[i].style.backgroundColor="brown"
+        text2[i].style.color="white"
+    }
+}
+function sendfun() {
+    var mess = document.querySelector(".messages").className=right;
+    var input = document.getElementById("input")
+    mess.append(input.value)
+    console.log("send")
+}
+var messageSide = 'left';
+var sendButton = document.getElementById("send-button")
+sendButton.addEventListener("click",function(event) {
+    event.preventDefault()
+    var mss = document.createElement("p")
+    var mes = document.getElementById("messages")
+    var inputBox = document.getElementById("input")
+    mss.textContent=inputBox.value
+    mss.className='message ' + messageSide
+    //switching sides if stgatment
+    if (messageSide === 'left') messageSide = 'right'; 
+    else messageSide = 'left';
+    mes.append(mss)
+})
